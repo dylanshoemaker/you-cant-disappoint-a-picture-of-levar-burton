7e2e41802da337a319f91e7a1c9a8948320d466e
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-20 13:37:52 -0800

    created schemas folder and define necessary query and mutation types

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,22 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+
+  Query: {
+    users: async () => {
+    return;
+    }
+  },
+
+  Mutation: {
+    addusers: async () => {
+      return;
+      }
+  },
+
+
+
+};
+module.exports = resolvers;
