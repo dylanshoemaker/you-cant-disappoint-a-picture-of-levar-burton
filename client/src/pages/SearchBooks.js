6150a1c90a092b7c10893988707b8bdcc78c8892
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-20 16:26:05 -0800

    update pages to handle apollo hooks to make graphql operations

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation } from "@apollo/client";
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import {SAVE_BOOK} from '../utils/mutations';
+
+
+
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -16,6 +21,10 @@ const SearchBooks = () => {
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+
+  const [saveBook] = useMutation(SAVE_BOOK)
+
+
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
@@ -65,7 +74,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: { input: bookToSave }
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
